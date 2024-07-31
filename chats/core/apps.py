89adcbdd1f8d3624666eba71740a8f2fd2d8de1e
Olalekan <asaajulekan@gmail.com>
2024-07-31 18:19:08 +0100

    Migration Made

--- chats/core/apps.py
+++ chats/core/apps.py
@@ -2,4 +2,9 @@ from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
-    default_auto_fiel
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core'
+
+
+# class CoreConfig(AppConfig):
+#     default_auto_fiel
